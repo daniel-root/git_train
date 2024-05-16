32ecfa046f795e2a4d98ff28727f238edec843ba
Daniel Albuquerque <albuquerquedaniel898@gmai.com>
2024-05-16 09:25:23 -0300

    rebase test
    file hello using class Animal
    
    fuc create dog
    
    Refactor:Change name (#1)
    
    Co-authored-by: Victor Wilker <wilker.santos@irede.org.br>

---
+++ hello.py
@@ -0,0 +1,12 @@
+from main import Animal
+
+cat = Animal(name='Xana', specie="Mamífero", sound="Miau")
+
+print(cat.make_noise())
+
+print(f"{cat.name} is {cat.specie}")
+
+def create_aminal():
+    dog = Animal(name='Janna', specie="Mamífero", sound="AuAu")
+    print(cat.make_noise())
+    
